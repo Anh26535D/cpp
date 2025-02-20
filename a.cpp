5e66044f74d4f6e4e49b816d24476ebb6fcf6169
Anh26535D <86581717+Anh26535D@users.noreply.github.com>
2025-02-20 23:57:44 +0700

    .

--- a.cpp
+++ a.cpp
@@ -1,15 +1,43 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <chrono>
+#include <execution>
 
 using namespace std;
 
+constexpr int kMaxIters = 1000;
+constexpr int kMaxSize = 100000000;
+
+void bm_innerproduct() {
+    auto start = chrono::steady_clock::now();
+    vector<int> v1(kMaxSize);
+    vector<int> v2(kMaxSize);
+    for (int i = 0; i < kMaxIters; ++i) {
+        int result = transform_reduce(
+            begin(v1), end(v1), begin(v2), 0
+        );
+    }
+    auto end = chrono::steady_clock::now();
+    cout << "inner_product: " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms" << endl;
+}
+
+void bm_partransformreduce() {
+    auto start = chrono::steady_clock::now();
+    vector<int> v1(kMaxSize);
+    vector<int> v2(kMaxSize);
+    for (int i = 0; i < kMaxIters; ++i) {
+        int result = transform_reduce(
+            execution::par_unseq,
+            begin(v1), end(v1), begin(v2), 0
+        );
+    }
+    auto end = chrono::steady_clock::now();
+    cout << "par_transform_reduce: " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms" << endl;
+}
+
 int main() {
-    std::vector<int> vect = {2, 4, 6, 8, 1, 3, 5, 7};
-    auto ppoint = partition_point(
-        begin(vect), 
-        end(vect), 
-        [](int i){return i % 2 == 0;}
-    );
-    cout << "The first odd value is " << *ppoint << endl;
+    bm_innerproduct();
+    bm_partransformreduce();
+    return 0;
 }
